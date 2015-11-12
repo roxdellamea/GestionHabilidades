62c6dc09fb997b5ec7d10445c3fe2c885cf5c78b
roxdellamea <roxdellamea@gmail.com>
2015-11-11 23:04:43 -0300

    modificacion de bundles

---
+++ src/GestionHabilidadesBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace GestionHabilidadesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('GestionHabilidadesBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/GestionHabilidadesBundle/GestionHabilidadesBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GestionHabilidadesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GestionHabilidadesBundle extends Bundle
+{
+}
